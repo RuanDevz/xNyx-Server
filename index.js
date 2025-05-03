505a6d459cff24b2bf8dc2868f7a219cb809fba0
ruandevz <Ruanbatista1509@outlook.com>
2025-05-03 01:04:13 -0300

    allow cors

--- index.js
+++ index.js
@@ -8,16 +8,9 @@ const { Pool } = require('pg');
 const app = express();
 
 app.use(express.json());
-const allowedOrigins = ['http://localhost:5173', 'https://xnyxleaks.com', 'https://x-nyx.vercel.app'];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: '*',
   methods: 'GET,POST,PUT,DELETE',
   credentials: true
 }));
