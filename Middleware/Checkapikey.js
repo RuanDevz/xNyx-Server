e572d09167aa0b98785eb674ddbed000bd6775d1
ruandevz <Ruanbatista1509@outlook.com>
2025-05-24 10:34:18 -0300

    autorize access

--- Middleware/Checkapikey.js
+++ Middleware/Checkapikey.js
@@ -4,15 +4,15 @@ module.exports = function checkApiKey(req, res, next) {
   const referer = req.headers.referer;
   const apiKey = req.headers['x-api-key'];
 
-  const allowedApiKey = process.env.VITE_FRONTEND_API_KEY;
-  const allowedOrigins = [
-    'https://xnyxleaks.com',
-    'http://localhost:5173',
-  ];
+  // const allowedApiKey = process.env.VITE_FRONTEND_API_KEY;
+  // const allowedOrigins = [
+  //   'https://xnyxleaks.com',
+  //   'http://localhost:5173',
+  // ];
 
-  const isAllowedOrigin =
-    allowedOrigins.includes(origin) ||
-    allowedOrigins.some(o => referer?.startsWith(o));
+  // const isAllowedOrigin =
+  //   allowedOrigins.includes(origin) ||
+  //   allowedOrigins.some(o => referer?.startsWith(o));
 
   if (apiKey !== allowedApiKey || !isAllowedOrigin) {
     return res.status(403).json({ error: 'Unauthorized access' });
